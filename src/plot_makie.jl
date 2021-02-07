9b90b585ef8ae57d01bf1498af1be62c94f0cc81
jleugeri <johannes@leugering.science>
2021-02-07 02:46:47 +0100

    i made a thing

--- src/plot_makie.jl
+++ src/plot_makie.jl
@@ -1,20 +1,17 @@
 using JSServe, GLMakie, AbstractPlotting, DataStructures
 import ColorTypes: RGB
 
-struct Port{W}
-    name::String
+struct Port{W,I}
+    name::I
 end
+Port{W}(name::I) where {W,I} = Port{W,I}(name)
 
-struct Branch
-    name::String
-    branches::Vector{Branch}
+struct Branch{I}
+    name::I
+    branches::Vector{Branch{I}}
     ports::Vector{Port}
 end
-
-struct Root
-    name::String
-    branches::Vector{Branch}
-end
+Branch(i::I, branches, ports) where I = Branch{I}(i, convert(Vector{Branch{I}},branches), ports)
 
 tree = Branch("Soma", [
     Branch("B1", [
@@ -61,26 +58,41 @@ tree = Branch("Soma", [
     ])
 ], [])
 
-function localize_branches(tree::Branch, l, angle_between)
+# if obj is an observable dict-type holding named values, get named value ...
+maybe_get(obj::Node, key) = if isa(obj[],Union{DefaultDict,Dict})
+    @lift $obj[key]
+else
+    obj
+end
+# if obj is a dict-type holding named values, get named value ...
+maybe_get(obj::Union{DefaultDict,Dict}, key) = obj[key]
+# ... otherwise return the obj itself
+maybe_get(obj, key) = obj
+
+function serialize_tree(tree::Branch{I}, l, ω) where {I}
     sectors = Float64[]
     depths = Float64[]
-    all_points = Vector{Pair{String,Vector{Point2f0}}}[]
-    all_points_flat = Pair{String,Vector{Point2f0}}[]
-    all_ports_flat = Pair{String,Vector{Port}}[tree.name=>tree.ports]
+    all_points = Vector{Pair{I,Point2f0}}[]
+    all_points_flat = Pair{I,Point2f0}[]
+    all_ports_flat = Pair{I,Vector{Port}}[tree.name=>tree.ports]
+    all_parents_flat = Pair{I,I}[]
 
     # go through all branches once to collect information
     for subtree in tree.branches
         # get angle and depth of sector
-        α,d,points,ports = localize_branches(subtree, l, angle_between)
+        α,d,points,ports,parents = serialize_tree(subtree, l, ω)
         # compute depth of new sector
-        c = √(l^2+d^2-2*l*d*cos(π-α/2))
+        ll = maybe_get(l, subtree.name)
+        c = √(ll^2+d^2-2*ll*d*cos(π-α/2))
         # compute angle of new sector
-        β=2acos((l^2+c^2-d^2)/(2l*c))
+        β=2acos((ll^2+c^2-d^2)/(2ll*c))
 
-        push!(sectors, β+angle_between)
+        push!(sectors, β+ maybe_get(ω, subtree.name))
         push!(depths, c)
         push!(all_points, points)
         append!(all_ports_flat, ports)
+        append!(all_parents_flat, parents)
+        push!(all_parents_flat, subtree.name=>tree.name)
     end
 
     # calculate total angle and depth for current (sub-)tree
@@ -97,23 +109,23 @@ function localize_branches(tree::Branch, l, angle_between)
         s=sin(branch_angle)
         transform = ((x,y),) -> (c*x-s*(y+l),s*x+c*(y+l))
 
-        # add segment for branch
-        push!(all_points_flat, branch.name=>[(0.0,0.0),transform((0.0,0.0))])
+        # add branch for branch
+        push!(all_points_flat, branch.name=>transform(Point2f0(0.0,0.0)))
         
-        # keep segments from branches' subtree
+        # keep branches from branches' subtree
         if !isempty(points)            
-            append!(all_points_flat, map(((name,pts),) -> name=>transform.(pts), points))
+            append!(all_points_flat, map(((name,pts),) -> name=>transform(pts), points))
         end
     end
 
-    return (sector=sector, depth=depth, segments=all_points_flat, ports=all_ports_flat)
+    return (sector=sector, depth=depth, nodes=all_points_flat, ports=all_ports_flat, parents=all_parents_flat)
 end
 
 function AbstractPlotting.default_theme(scene::AbstractPlotting.SceneLike, ::Type{<: AbstractPlotting.Plot(Branch)})
     Theme(
         color=RGB(0.9,0.9,0.9),
-        segment_width=10,
-        segment_length=1.0,
+        branch_width=0.1,
+        branch_length=1.0,
         angle_between=10/180*pi,
         show_port_labels=true,
         xticks = [],
@@ -123,44 +135,76 @@ function AbstractPlotting.default_theme(scene::AbstractPlotting.SceneLike, ::Typ
 end
 
 function AbstractPlotting.plot!(treeplot::AbstractPlotting.Plot(Branch))
+    # get the actual object to plot
     tree = to_value(treeplot[1])
-    port_marker = to_value(treeplot[:port_marker])
-    port_label = to_value(treeplot[:port_label])
 
-    serialized = localize_branches(tree,to_value(treeplot[:segment_length]),to_value(treeplot[:angle_between]))
-    segments = serialized[:segments]
-    ports = Dict(serialized[:ports]...)
-    ports_locations = Pair{String,Point2f0}[]
-
-    # plot segments
-    for (name,segment) in segments
-        cval = to_value(treeplot[:color])
-        c = isa(cval, Union{DefaultDict,Dict}) ? cval[name] : treeplot[:color]
-        lines!(treeplot, segment, linewidth=treeplot[:segment_width], color=c)
-        scatter!(treeplot, segment, markersize=treeplot[:segment_width], color=c, strokewidth=0)
+    # get all nodes, their ports and parents in flat format
+    serialized = lift(treeplot[:branch_length],treeplot[:angle_between]) do l,ω
+        ser=serialize_tree(tree,l,ω)
+        push!(ser.nodes, tree.name => Point2f0(0.0,0.0))
+        ser
     end
+    parent = DefaultDict(tree.name, serialized[].parents...)
 
-    # plot root
-    cval = to_value(treeplot[:color])
-    c = isa(cval, Union{DefaultDict,Dict}) ? cval[tree.name] : treeplot[:color]
-    poly!(treeplot, Point2f0[(-0.25,-0.25),(0.25,-0.25),(0.0, 0.25)], color=c)
-    lines!(treeplot, Point2f0[(0.0, 0.0),(0.0, -0.5)], linewidth=treeplot[:segment_width], color=c)
+    # plot branches
+    for (name,parent_name) in serialized[].parents
+        c  = maybe_get(treeplot[:color], name)
+        w1 = maybe_get(treeplot[:branch_width], name)
+        w2 = maybe_get(treeplot[:branch_width], parent_name)
 
-    # plot ports & labels
-    for (name,segment) in segments
-        for (port,loc) in zip(ports[name], LinRange(segment..., length(ports[name])+2)[2:end-1])
-            scatter!(treeplot, [loc]; port_marker(port)...)
-            label = port_label(port)
-            text,offset = label[1:2]
-            args = label[3:end] 
-            
-            text!(treeplot, text, position=loc+offset, visible=treeplot[:show_port_labels]; args...)
-            push!(ports_locations, port.name=>loc)
+        # dynamically recompute polygon
+        branch_poly = lift(w1, w2, serialized) do w1,w2,ser
+            node_dict = Dict(ser.nodes...)
+            b1 = node_dict[name]
+            b2 = node_dict[parent_name]
+            normal = [0 1; -1 0]*(b2-b1)
+            normal /= sqrt(normal'*normal)*2
+            Point2f0[
+                -normal*w1+b1,
+                 normal*w1+b1,
+                 normal*w2+b2,
+                -normal*w2+b2
+            ]
         end
+        
+        # draw polygon for the branch
+        poly!(treeplot, branch_poly, color=c)
+
+        # draw circle to cap off branch
+        poly!(treeplot, lift(w2,serialized) do w2,ser
+            node_dict = Dict(ser.nodes...)
+            b2 = node_dict[name]
+            Circle(b2, w2/2)
+        end, color=c)
     end
+
+    # draw polygon for the root
+    c = maybe_get(treeplot[:color], tree.name)
+    w = maybe_get(treeplot[:branch_width], tree.name)
+    root_poly = lift(w) do w
+        Point2f0[(-w/2*√(3), -w/2), (0,w), (w/2*√(3), -w/2)]
+    end
+    println(c)
+    poly!(treeplot, root_poly, color=c)
+    # lines!(treeplot, Point2f0[(0.0, 0.0),(0.0, -0.5)], linewidth=treeplot[:branch_width], color=c)
+
+    # ports_locations = Pair{String,Point2f0}[]
+
+    # # plot ports & labels
+    # for (name,branch) in branches
+    #     for (port,loc) in zip(ports[name], LinRange(branch..., length(ports[name])+2)[2:end-1])
+    #         scatter!(treeplot, [loc]; port_marker(port)...)
+    #         label = port_label(port)
+    #         text,offset = label[1:2]
+    #         args = label[3:end] 
+            
+    #         text!(treeplot, text, position=loc+offset, visible=treeplot[:show_port_labels]; args...)
+    #         push!(ports_locations, port.name=>loc)
+    #     end
+    # end
     
 
-    treeplot.attributes[:ports] = ports_locations
+    # treeplot.attributes[:ports] = ports_locations
     treeplot
 end
 
@@ -168,19 +212,27 @@ scene, layout = layoutscene(resolution = (1200, 900))
 ax = layout[1, 1] = Axis(scene, title="Neuron", autolimitaspect=1)
 branch_names = ["Soma", "B1", "B2", "B21", "B22", "B221", "B222", "B223", "B224", "B225", "B226", "B3", ]
 
-color = Dict(name=>Observable(rand(RGB)) for name in branch_names)
+color = Node(Dict(name=>rand(RGB) for name in branch_names))
 #DefaultDict(Observable(RGB(0.9,0.9,0.9)), "B22" => Observable(RGB(0.9,0.1,0.1)))
 
-plot!(ax, tree, angle_between=10/180*π, color=color, show_port_labels=false)
+ls = labelslidergrid!(scene, ["Inner angle", "Width"], [0.01:0.01:1π, 0.001:0.001:0.25]; format = x -> "$(round(x,digits=1))")
+angle_between = ls.sliders[1].value
+branch_width = ls.sliders[2].value
+plot!(ax, tree, angle_between=angle_between, branch_width=branch_width, color=color, show_port_labels=false)
 hidespines!(ax)
 hidedecorations!(ax)
-ports = to_value(plt.attributes[:ports])
+# ports = to_value(plt.attributes[:ports])
 #lines!(ax, Point2f0[(-1,3), ports[3][2]])
-c = cgrad(:magma)
 
+layout[2, 1] = ls.layout
+display(scene)
+
+c = cgrad(:magma)
 record(scene, "blinky_neuron.mp4", LinRange(0,2*pi,1000); framerate=30) do t
     print(".")
     for (i,name) in enumerate(branch_names)
-        color[name][] = c[sin(i*t)/2+0.5]
+        color[][name] = c[sin(i*t)/2+0.5]
     end
+    # workaround: force update on color
+    color[] = color[]
 end
